25808e2b320046745759b70324d49acb86401a11
fakorede <abiolafakorede@fmail.com>
2021-12-27 18:20:28 +0100

    add migrations for seeding rooms and restrictions table

---
+++ migrations/20211227170638_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+  ('General Quarters','2021-08-23 00:00:00.000','2021-08-23 00:00:00.000'),
+  ('Major Suites','2021-12-27 00:00:00.000','2021-12-27 00:00:00.000');
